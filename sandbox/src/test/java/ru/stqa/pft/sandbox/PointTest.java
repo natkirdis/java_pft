1414b122e9889010e2d8f24cf3c2c6d8514f8d86
Natalia Kirdis <kirdisnatalia@gmail.com>
2019-08-05 22:34:59 +0300

    Исправлен метод расчета расстояния между точками согласно комментарию

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTest.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTest.java
@@ -8,27 +8,27 @@ public class PointTest {
   public void testPointPositive() {
     Point p1 = new Point(1, 2);
     Point p2 = new Point(3, 6);
-    Assert.assertEquals(p1.distance(p1, p2),4.47213595499958);
+    Assert.assertEquals(p1.distance(p2),4.47213595499958);
   }
 
   @Test
   public void testPointDistanceIsNull() {
     Point p1 = new Point(1, 2);
     Point p2 = new Point(1, 2);
-    Assert.assertEquals(p1.distance(p1, p2),0);
+    Assert.assertEquals(p1.distance(p2),0);
   }
 
   @Test
   public void testPointDistanceSecondIsLess() {
     Point p1 = new Point(1, 2);
     Point p2 = new Point(0, 0);
-    Assert.assertEquals(p1.distance(p1, p2),2.23606797749979);
+    Assert.assertEquals(p1.distance(p2),2.23606797749979);
   }
 
   @Test
   public void testPointDistanceFirstIsLess() {
     Point p1 = new Point(-3, -777);
     Point p2 = new Point(0, 0);
-    Assert.assertEquals(p1.distance(p1, p2),777.0057914842076);
+    Assert.assertEquals(p1.distance(p2),777.0057914842076);
   }
 }
