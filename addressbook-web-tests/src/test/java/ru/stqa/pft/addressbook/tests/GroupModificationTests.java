11b6f177fdcfaadba3596b92af6e511d94321ad9
Natalia Kirdis <kirdisnatalia@gmail.com>
2019-08-17 09:40:31 +0300

    Добавление проверки в метод type(), что вводимый текст не совпадает с существующим текстом

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -10,7 +10,7 @@ public class GroupModificationTests extends TestBase {
     app.getNavigationHelper().gotoGroupPage();
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().initGroupModification();
-    app.getGroupHelper().fillGroupForm(new GroupData("edited", "edited2", "edited3"));
+    app.getGroupHelper().fillGroupForm(new GroupData("test1", "edited2", "edited3"));
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
   }
