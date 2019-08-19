b56ccc3aa0e8819674ebd9337695cde5c07b6b91
Natalia Kirdis <kirdisnatalia@gmail.com>
2019-08-19 07:41:42 +0300

    При переходе на страницу добавлена проверка, что мы не находимся на данной странице

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -3,16 +3,27 @@ package ru.stqa.pft.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class NavigationHelper extends HelperBase{
+public class NavigationHelper extends HelperBase {
 
   public NavigationHelper(WebDriver wd) {
     super(wd);
   }
 
   public void gotoGroupPage() {
-    click(By.linkText("groups"));
+    if (isElementPresent(By.tagName("h1"))
+            && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+            && isElementPresent(By.name("new"))) {
+      return;
+    } else {
+      click(By.linkText("groups"));
+    }
   }
+
   public void gotoContactPage() {
-    click(By.linkText("home"));
+    if (isElementPresent(By.id("maintable"))) {
+      return;
+    } else {
+      click(By.linkText("home"));
+    }
   }
 }
