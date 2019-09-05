fc7cdeff12304bb3f413a5a4e9df96ad9818cdcd
Natalia Kirdis <kirdisnatalia@gmail.com>
2019-09-05 20:55:40 +0300

    [Contacts]: Тесты для проверки email и телефонов объединены в один

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactPhoneEmailTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactPhoneEmailTests.java
@@ -10,7 +10,7 @@ import java.util.stream.Collectors;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-public class ContactPhoneTests extends TestBase {
+public class ContactPhoneEmailTests extends TestBase {
 
   @BeforeMethod
   public void ensurePreconditions() {
@@ -19,23 +19,31 @@ public class ContactPhoneTests extends TestBase {
       app.contact().create(new ContactData()
               .withFirstName("test name").withMiddleName("test middle name").withLastName("test last name")
               .withHomePhone("89993424433").withWorkPhone("+7(943)273-33-01").withMobilePhone("242-09-21")
-              .withEmail("test@email.ru").withGroup("test1"), true);
+              .withEmail("test@email.ru").withEmail1("шла саша по шоссе").withEmail2("&^563")
+              .withGroup("test1"), true);
     }
   }
 
   @Test
-  public void testContactPhones() {
+  public void testContactPhonesEmails() {
     app.goTo().сontactPage();
     ContactData contact = app.contact().all().iterator().next();
     ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
 
     assertThat(contact.getAllPhones(), equalTo(mergePhones(contactInfoFromEditForm)));
+    assertThat(contact.getAllEmails(), equalTo(mergeEmails(contactInfoFromEditForm)));
   }
 
   private String mergePhones(ContactData contact) {
     return Arrays.asList(contact.getHomePhone(), contact.getMobilePhone(), contact.getWorkPhone())
             .stream().filter((s) -> !s.equals(""))
-            .map(ContactPhoneTests::cleaned)
+            .map(ContactPhoneEmailTests::cleaned)
+            .collect(Collectors.joining("\n"));
+  }
+
+  private String mergeEmails(ContactData contact) {
+    return Arrays.asList(contact.getEmail(), contact.getEmail1(), contact.getEmail2())
+            .stream().filter((s) -> !s.equals(""))
             .collect(Collectors.joining("\n"));
   }
 
